32c26f250a4c160dc74ada59b0da600fd3d8a794
Mubeshira <159130265+Mubeshira@users.noreply.github.com>
2024-04-04 13:00:13 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// function
+function burger(patty, cheeseSlice) {
+    var bun = 2;
+    return bun + patty + cheeseSlice;
+}
+// Mubeshira 2 patties // 5
+var result_1 = (burger(2, 1));
+console.log(result_1);
+//  Ambreen 1 patty // 5
+var result_2nd = (burger(1, 2));
+console.log(result_2nd);
+// Alveera 3 patties // 8
+var result_3rd = burger(3, 3);
+console.log(result_3rd);
